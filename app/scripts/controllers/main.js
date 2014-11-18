5880ef49ffbdef36605bb53bd8edc01e48d331be
cmarkel5 <cmarkel5@gmail.com>
2014-11-18 18:20:49 -0500

    angular map working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,16 +15,47 @@ angular.module('connectusAngular2App')
       'Karma'
     ];
 
+    var midPointLat = (33.882147 + 33.773164)/2
+    var midPointLong = (-84.521571 + -84.288918)/2
+
     $scope.map = {
       center: {
-        latitude: 33.771112,
-        longitude: -84.367090
+        latitude: midPointLat,
+        longitude: midPointLong
       },
-      zoom: 12
+      zoom: 10
     };
 
     uiGmapGoogleMapApi.then(function(/* maps */) {
       // can manipulate the map here.
     });
+
+  $scope.markerList = [
+    // {
+    //   id: 0,
+    //   coords: {
+    //     latitude: 33.771527,
+    //     longitude: -84.367367
+    //   },
+    //   message: 'General Assembly'
+    // },
+    {
+      id: 1,
+      coords: {
+        latitude: 33.882147,
+        longitude: -84.521571
+      },
+      message: 'Chris Markel'
+      // icon:"https://en.gravatar.com/userimage/74172252/39d87c7514bfdc6e8bcf4d7b8fc35472.jpg"
+    },
+    {
+      id: 2,
+      coords: {
+        latitude: 33.773164,
+        longitude: -84.288918
+      },
+      message:'Amanda Raymond'
+    }
+  ]
 
   });
